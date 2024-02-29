874985f932e450f7c963c10dd715f06cde04e3cb
Alberto Sousa <albertovssousa@gmail.com>
2024-02-29 17:31:40 -0300

    Lobby Part 2 - NetworkRunner

--- FusionTutorial/Assets/Scripts/Lobby/MiddleSectionPanel.cs
+++ FusionTutorial/Assets/Scripts/Lobby/MiddleSectionPanel.cs
@@ -1,8 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Fusion;
 
 public class MiddleSectionPanel : LobbyPanelBase
 {
+    [Header("Middle Section Panel vars")]
+    [SerializeField] private Button joinRandomRoomBtn;
+    [SerializeField] private Button joinRoomByArgBtn;
+    [SerializeField] private Button createRoomBtn;
+    [SerializeField] private TMP_InputField joinRoomByArgInputField;
+    [SerializeField] private TMP_InputField createRoomInputField;
 
+    private NetworkRunnerController networkRunnerController;
+
+    public override void InitPanel(LobbyUIManager lobbyUIManager)
+    {
+        base.InitPanel(lobbyUIManager);
+
+        networkRunnerController = GlobalManagers.Instance.NetworkRunnerController;
+        joinRandomRoomBtn.onClick.AddListener(JoinRandomRoom);
+        joinRoomByArgBtn.onClick.AddListener(() => CreateRoom(GameMode.Client, joinRoomByArgInputField.text));
+        createRoomBtn.onClick.AddListener(() => CreateRoom(GameMode.Host, createRoomInputField.text));
+    }
+
+    private void CreateRoom(GameMode mode, string roomName)
+    {
+        if(roomName.Length >= 2)
+        {
+            Debug.Log($"----------------{mode}--------------------");
+            networkRunnerController.StartGame(mode, roomName);
+        }
+    }
+
+    private void JoinRandomRoom()
+    {
+        Debug.Log($"----------------JoinRandomRoom!--------------------");
+        networkRunnerController.StartGame(GameMode.AutoHostOrClient, string.Empty);
+    }
 }
---
+++ FusionTutorial/Assets/Scripts/Other/NetworkRunnerController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+
+public class NetworkRunnerController : MonoBehaviour, INetworkRunnerCallbacks
+{
+    [SerializeField] private NetworkRunner networkRunnerPrefab;
+
+    private NetworkRunner networkRunnerInstance;
+
+    public async void StartGame(GameMode mode, string roomName)
+    {
+        if(networkRunnerInstance == null)
+        {
+            networkRunnerInstance = Instantiate(networkRunnerPrefab);
+        }
+
+        //Register so we will get the callbacks as well
+        networkRunnerInstance.AddCallbacks(this);
+
+        //networkRunnerInstance.ProvideInput = true;
+
+        var startGameArgs = new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = roomName,
+            PlayerCount = 4,
+            SceneManager = networkRunnerInstance.GetComponent<INetworkSceneManager>()
+        };
+
+        var result = await networkRunnerInstance.StartGame(startGameArgs);
+
+        if(result.Ok)
+        {
+            const string SCENE_NAME = "MainGame";
+            networkRunnerInstance.SetActiveScene(SCENE_NAME);
+        }
+        else
+        {
+            Debug.Log($"Failed to start: {result.ShutdownReason}");
+        }
+    }
+
+
+ 	//Callback when NetworkRunner successfully connects to a server or host.
+    public void OnConnectedToServer (NetworkRunner runner)
+    {Debug.Log("OnConnectedToServer");}
+ 
+ 	//Callback when NetworkRunner fails to connect to a server or host.
+    public void OnConnectFailed (NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {Debug.Log("OnConnectFailed");}
+ 
+ 	//Callback when NetworkRunner receives a Connection Request from a Remote Client.
+    public void OnConnectRequest (NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {Debug.Log("OnConnectRequest");}
+ 
+ 	//Callback is invoked when the Authentication procedure returns a response from the Authentication Server.
+    public void OnCustomAuthenticationResponse (NetworkRunner runner, Dictionary< string, object > data)
+    {Debug.Log("OnCustomAuthenticationResponse");}
+ 
+ 	//Callback when NetworkRunner disconnects from a server or host.
+	public void OnDisconnectedFromServer (NetworkRunner runner)
+    {Debug.Log("OnDisconnectedFromServer");}
+ 	
+    //Callback is invoked when the Host Migration process has started.
+	public void OnHostMigration (NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {Debug.Log("OnHostMigration");}
+	
+ 	//Callback from NetworkRunner that polls for user inputs. The NetworkInput that is supplied expects:
+    public void OnInput (NetworkRunner runner, NetworkInput input)
+    {Debug.Log("OnInput");}
+	
+    public void OnInputMissing (NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {Debug.Log("OnInputMissing");}
+ 	
+    // Callback from a NetworkRunner when a new player has joined.
+	public void OnPlayerJoined (NetworkRunner runner, PlayerRef player)
+    {Debug.Log("OnPlayerJoined");}
+ 	
+    //Callback from a NetworkRunner when a player has disconnected.
+	public void OnPlayerLeft (NetworkRunner runner, PlayerRef player)
+    {Debug.Log("OnPlayerLeft");}
+	
+    public void OnReliableDataReceived (NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
+    {Debug.Log("OnReliableDataReceived");}
+
+	public void OnSceneLoadDone (NetworkRunner runner)
+    {Debug.Log("OnSceneLoadDone");}
+
+	public void OnSceneLoadStart (NetworkRunner runner)
+    {Debug.Log("OnSceneLoadStart");}
+
+ 	//This callback is invoked when a new List of Sessions is received from Photon Cloud.
+	public void OnSessionListUpdated (NetworkRunner runner, List<SessionInfo> sessionList)
+    {Debug.Log("OnSessionListUpdated");}
+
+ 	//Called when the runner is shutdown.
+	public void OnShutdown (NetworkRunner runner, ShutdownReason shutdownReason)
+    {Debug.Log("OnShutdown");}
+
+	public void OnUserSimulationMessage (NetworkRunner runner, SimulationMessagePtr message)
+    {Debug.Log("OnUserSimulationMessage");}
+}
